21d73d78c7374f161e3c1542b72f597538dc01aa
Sarah Gonzalez. <sarahgonzalez905@gmail.com>
2025-05-14 20:59:33 -0300

    Add files via upload
    como funciona os inputs.

---
+++ Inputsequence.py
@@ -0,0 +1,27 @@
+#input: user typing and interacting with software - test - aprendizado
+
+name = input ('Hello! please, type your name: ')
+age = input ('type your age:')
+country = input ('type your country: ')
+
+print (f'''Very nice to meet you {name}. Nice! your {age} is 
+very beautiful, i liked {country}, lol''')
+
+# como descobrir o tipo de variável que é utilizada
+
+nome = input ('Digite seu nome:')      #está entre aspas por isso é string''
+idade = int(input ('digite sua idade:')) # variavel int por receber um número inteiro
+peso = float(input('digite seu peso:'))
+
+# acima vemos convenções de dados
+
+print (type(nome))  #type;String - texto
+print (type(idade)) #type;Int - inteiro
+print (type(peso)) #type;float - decimal
+
+# a função "type" define a qual variavel pertence as informações que há presentes
+
+
+
+
+
